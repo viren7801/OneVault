f4432b2873b3ad639cb6a7f4c56c15485fb73e9c
viren7801 <127821519+viren7801@users.noreply.github.com>
2026-09-19 23:52:59 +0530

    fix: enable automatic background live updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -10,7 +10,7 @@ const config: CapacitorConfig = {
   },
   plugins: {
     LiveUpdate: {
-      autoUpdateStrategy: 'none',
+      autoUpdateStrategy: 'background',
       autoBlockRolledBackBundles: true,
       readyTimeout: 10000,
     },
