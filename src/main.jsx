215b496511f12f183bb957b3951ba4d2581ccbd5
viren7801 <127821519+viren7801@users.noreply.github.com>
2026-09-19 17:10:26 +0530

    feat: add in-app update center

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import UpdateCenter from './UpdateCenter';
 import './styles.css';
+import './updateCenter.css';
 import { initializeLiveUpdates } from './lib/liveUpdates';
 
 if ('serviceWorker' in navigator) {
@@ -15,9 +17,9 @@ if ('serviceWorker' in navigator) {
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <UpdateCenter />
   </React.StrictMode>
 );
-
 
 window.addEventListener('load', () => {
   void initializeLiveUpdates();
